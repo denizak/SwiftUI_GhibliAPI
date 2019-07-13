b69af05a8d88b1ed6760186e83b4690e3206c3d1
Deni Zakya <denizakya@gmail.com>
2019-07-13 14:43:59 +0700

    Initial Commit

---
+++ ghibliapp/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  ghibliapp
+//
+//  Created by Deni Zakya on 13/07/19.
+//  Copyright © 2019 Deni Zakya. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
